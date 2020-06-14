b07ddc4d71b40be4b1a7b763cbb69859094b4cab
Steve Kemp <steve@steve.org.uk>
2020-06-14 12:29:53 +0300

    Drive GCC programatically, allow running too.

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
 // Given an input brainfuck program generate a comparable C version.
-func compile(source string) string {
+//
+// Return the filename we generated, or error
+func generateProgram(source string) (string, error) {
 	var buff bytes.Buffer
 	var programStart = `
 	extern int putchar(int);
@@ -47,36 +51,90 @@ func compile(source string) string {
 	}
 
 	buff.WriteString("}\n")
-	return buff.String()
+
+	tmpfile, err := ioutil.TempFile("", "bfcc*.c")
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := tmpfile.Write(buff.Bytes()); err != nil {
+		return "", err
+	}
+	if err := tmpfile.Close(); err != nil {
+		return "", err
+	}
+
+	return tmpfile.Name(), nil
 }
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Printf("Usage: bfcc <infile> [<outfile>]\n")
+
+	//
+	// Parse command-line flags
+	//
+	compile := flag.Bool("compile", true, "Compile the generated C, after generation")
+	run := flag.Bool("run", false, "Run the program after compiling")
+	flag.Parse()
+
+	//
+	// Get the input filename
+	//
+	if len(flag.Args()) < 1 {
+		fmt.Printf("Usage: bfcc [flags] input.file.bf [outfile]\n")
 		return
 	}
 
-	input := os.Args[1]
-	output := "out.c"
-	if len(os.Args) == 3 {
-		output = os.Args[2]
+	//
+	// Input and output files
+	//
+	input := flag.Args()[0]
+	output := "a.out"
+	if len(flag.Args()) == 2 {
+		output = os.Args[1]
 	}
 
-	// Read input
+	//
+	// Read the input program.
+	//
 	prog, err := ioutil.ReadFile(input)
 	if err != nil {
-		fmt.Printf("Failed to read input file %s: %s\n", input, err.Error())
+		fmt.Printf("failed to read input file %s: %s\n", input, err.Error())
 		return
 	}
 
+	//
 	// "Compile"
-	out := compile(string(prog))
-
-	// Write output
-	err = ioutil.WriteFile(output, []byte(out), 0644)
+	//
+	path, err := generateProgram(string(prog))
 	if err != nil {
-		fmt.Printf("failed to write to %s: %s\n", output, err.Error())
+		fmt.Printf("error writing output: %s\n", err.Error())
 		return
 	}
 
+	//
+	// Compile
+	//
+	if *compile {
+		gcc := exec.Command("gcc", "-O3", "-Ofast", "-o", output, path)
+		gcc.Stdout = os.Stdout
+		gcc.Stderr = os.Stderr
+
+		err = gcc.Run()
+		if err != nil {
+			fmt.Printf("Error launching gcc: %s\n", err)
+			return
+		}
+	}
+
+	if *run {
+		exe := exec.Command(output)
+		exe.Stdout = os.Stdout
+		exe.Stderr = os.Stderr
+		err = exe.Run()
+		if err != nil {
+			fmt.Printf("Error launching %s: %s\n", output, err)
+			os.Exit(1)
+		}
+
+	}
 }
